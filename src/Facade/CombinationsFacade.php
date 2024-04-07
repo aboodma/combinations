f2794b22b5e549d67266a26b5269789ae12f6d59
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2024-04-07 17:27:50 +0300

    Init

---
+++ src/Facade/CombinationsFacade.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Aboodma\Combinations\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \Laracon21\Combinations\Skeleton\SkeletonClass
+ */
+class CombinationsFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'combinations';
+    }
+}
